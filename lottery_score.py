d24c2b3f73a97cb0f288227d7faf8dfa6a53c9ae
inyourcare <inyourcareallen@gmail.com>
2018-11-12 12:34:32 +0900

    branch

--- lottery_score.py
+++ lottery_score.py
@@ -1,39 +1,18 @@
 from iconservice import *
 
-TAG = 'SampleToken'
+TAG = 'lottery_score'
 
 
-class CrowdSaleInterface(InterfaceScore):
-    @interface
-    def tokenFallback(self, _from: Address, _value: int, _data: bytes):
-        pass
+class Lottery(IconScoreBase):
 
-
-class StandardToken(IconScoreBase):
-
-    _BALANCES = 'balances'
-    _TOTAL_SUPPLY = 'total_supply'
-    _DECIMALS = 'decimals'
-
-    @eventlog(indexed=3)
-    def Transfer(self, _from: Address, _to: Address, _value: int, _data: bytes):
-        pass
 
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
-        self._total_supply = VarDB(self._TOTAL_SUPPLY, db, value_type=int)
-        self._decimals = VarDB(self._DECIMALS, db, value_type=int)
-        self._balances = DictDB(self._BALANCES, db, value_type=int)
 
     def on_install(self, initialSupply: int, decimals: int) -> None:
         super().on_install()
-
-        total_supply = initialSupply * 10 ** decimals
-        Logger.debug(f'on_install: total_supply={total_supply}', TAG)
-
-        self._total_supply.set(total_supply)
-        self._decimals.set(decimals)
-        self._balances[self.msg.sender] = total_supply
+        self._totalPrize
+        self._
 
     def on_update(self) -> None:
         super().on_update()
